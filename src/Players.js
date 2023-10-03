62454ac899e988d79bf2e8d1785383e4ff79f69c
khosi <cosmasndulue00611@gmail.com>
2023-10-03 07:20:39 +0100

    images

---
+++ src/Players.js
@@ -0,0 +1,15 @@
+        import React from 'react'
+
+        const Players = () => {
+        return (
+            <div>
+                <div>
+                    <h1>bruno Fernandez </h1>
+            <img src="./images/bruno-fernandes.jpeg" alt="bruno" />
+                </div>
+            </div>
+        )
+        }
+
+        export default Players
+
